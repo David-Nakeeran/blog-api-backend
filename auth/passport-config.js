dc248965500952885cc6ae953b0a2149df296711
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-05 17:02:45 +0100

    authentication strategy

---
+++ auth/passport-config.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const User = require('../models');
+
+// Passport Local Strategy
+passport.use(
+    new LocalStrategy(async(username, password, done) => {
+        try {
+            const user = User.findOne({username: username});
+            if(!user) {
+                return done(null, false, {message: "Incorrect username"});
+            }
+            const match = await bcrypt.compare(password, user.password);
+            if(!match) {
+                return done(null, false, {message: "Incorrect password"})
+            }
+            return done(null, user);
+        } catch(error) {
+            return done(error)
+        };
+    })
+);
+
+// Serialise user
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+// Deserialise user
+passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch(err) {
+      done(err);
+    };
+});
