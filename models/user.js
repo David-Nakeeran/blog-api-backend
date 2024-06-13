21617022790663c3da39807b8d9385aaa632b5cf
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-13 11:26:44 +0100

    Set virtual fields as JSON and toObject responses  for User schema

--- models/user.js
+++ models/user.js
@@ -14,6 +14,9 @@ UserSchema.virtual("fullName").get(function() {
     return `${this.firstName} ${this.surname}`
 })
 
+// Enables virtuals in JSON and Object responses
+UserSchema.set('toObject', {virtuals: true});
+UserSchema.set('toJSON', {virtuals: true});
 // UserSchema.virtual("url").get(function() {
 //     return `/user/${this._id}`;
 // });
