d8c4cab803d5cb8c0b2fcd2ffd2f8cc6d74cd655
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-13 11:22:36 +0100

    Changed index route to GET all posts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const postController = require('../controllers/postController');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// Get all POSTS
+router.get('/', postController.postList);
+
 
 module.exports = router;
