4f9111a455718b848d82379da57ce99c9be195c5
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-13 11:21:32 +0100

    Added post routes for Get post detail and Get post detail comments

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const postController = require('../controllers/postController');
+const commentController = require('../controllers/commentController');
 const {authenticateAdminToken} = require('../auth/authenticate-jwt')
 
 // Create post 
 router.post("/", authenticateAdminToken, postController.postCreate);
+
+// Get post detail
+router.get("/:id", postController.postDetail);
+
+// Create post comment
+router.post("/:id/comments", authenticateAdminToken, commentController.commentCreate);
 
 module.exports = router;
