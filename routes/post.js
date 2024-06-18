4e2c2044eb8ef3d40c0bb68816cee6ea01cc10d5
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-18 13:05:06 +0100

    Added all associated post controller routes.

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const postController = require('../controllers/postController');
 const commentController = require('../controllers/commentController');
-const {authenticateAdminToken} = require('../auth/authenticate-jwt')
+const {authenticateAdminToken, authenticateToken} = require('../auth/authenticate-jwt')
+
+// Display all posts Admin
+router.get("/", authenticateAdminToken, postController.postAdminList)
 
 // Create post 
 router.post("/", authenticateAdminToken, postController.postCreate);
@@ -10,7 +13,23 @@ router.post("/", authenticateAdminToken, postController.postCreate);
 // Get post detail
 router.get("/:id", postController.postDetail);
 
+// Admin delete post
+router.delete("/:id", authenticateAdminToken, postController.postAdminDelete)
+
+// Admin update isPublished post
+router.patch("/:id", authenticateAdminToken, postController.postAdminPublish);
+
+// Admin update post content
+router.put("/:id", authenticateAdminToken, postController.postAdminUpdatePost);
+
 // Create post comment
-router.post("/:id/comments", authenticateAdminToken, commentController.commentCreate);
+router.post("/:id/comments", authenticateToken, commentController.commentCreate);
+
+// User delete own comment
+router.delete("/:id/comments/:commentId", authenticateToken, commentController.commentUserDelete)
+
+// Admin delete any comment
+router.delete("/:id/comments/:commentId", authenticateAdminToken, commentController.commentAdminDelete)
+
 
 module.exports = router;
