28aac31677c361a0716ecada53ebe36ee4b92022
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-18 12:45:32 +0100

    Updated postList to only display posts that have isPublished === true. Also only populating author firstName, surname and fullName fields to be sent via json.
    Added postAdminList, same as postList except
    displays all posts.
    
    Added postAdminDelete function, enabling admin to
    delete blog posts and any associated comments.
    
    Added postAdminPublish function, allowing admin to
     set isPublished field to true or false.
    
    Added postAdminUpdatePost function, enables admin
    to update blog posts.

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,22 +3,59 @@ const Comment = require('../models/comment');
 const asyncHandler = require('express-async-handler');
 const {body, validationResult} = require('express-validator');
 
-// Display all POSTS 
+/* To DO
+1. Change all reference of .populate('author') with - .populate({
+                    path: 'author',
+                    select: 'firstName surname fullName'
+            })
+2. Remove try catch blocks
+3. Create helper function/ middleware for repetitive error handling
+*/
+
+// Display all POSTS
 exports.postList = asyncHandler(async(req, res, next) => {
-    try {
-        // To add = find{isPublished: true}
-        const allPosts = await Post.find({}, "author")
-            .populate("author")
+    
+    const allPublishedPosts = await Post.find({isPublished: true}, "author")
+            .populate({
+                    path: 'author',
+                    select: 'firstName surname fullName'
+            })
             .sort({createdAt: -1})
             .exec()
+    
+    if(!allPublishedPosts) {
+        const err = new Error('Posts not found');
+        err.status = 404;
+        return next(err)
+    }
 
-        res.status(200).json({
+    res.status(200).json({
+        status: "success",
+        posts: allPublishedPosts
+    })
+    
+    
+})
+
+// Display all POSTS Admin
+exports.postAdminList = asyncHandler(async(req, res, next) => {
+    
+    const allPosts = await Post.find({}, "author")
+        .populate("author")
+        .sort({createdAt: -1})
+        .exec()
+
+    if(!allPosts) {
+        const err = new Error('Posts not found');
+        err.status = 404;
+        return next(err)
+    }
+        
+    res.status(200).json({
         status: "success",
         posts: allPosts
     })
-    }   catch(err) {
-            return next(err)
-    }
+    
 })
 
 // Handle article POST
@@ -98,3 +135,117 @@ exports.postDetail = asyncHandler(async(req, res, next) => {
         return next(err)
     }
 })
+
+// Delete specific post
+exports.postAdminDelete = asyncHandler(async(req, res, next) => {
+    // Find post to ensure it exists
+    const post = await Post.findById(req.params.id)
+        .populate('author')
+        .exec()
+
+    if(!post) {
+        return res.status(404).json({
+            status: "Error",
+            message: "Post not found"
+        });
+    }
+
+    // Delete all comments associated with the specific post
+    await Comment.deleteMany({post: post._id});
+
+    // Delete the post
+    await Post.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({
+        status: "Success",
+        message: "Post and associated comments deleted successfully"
+    });
+});
+
+// Admin publish and unpublish posts
+exports.postAdminPublish = asyncHandler(async(req, res, next) => {
+
+    const post = await Post.findByIdAndUpdate(req.params.id, {isPublished: req.body.isPublished}, {new: true})
+
+    if(!post) {
+        const err = new Error("Post not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.status(200).json({
+        status: 'success',
+        message: 'Post status updated successfully',
+        post: post
+    })
+});
+
+
+exports.postAdminUpdatePost = [
+    // Validate and sanitise fields
+    body("title")
+        .optional()
+        .trim()
+        .isLength({min: 1})
+        .escape()
+        .withMessage("Title must be specified"),
+    body("text")
+        .optional()
+        .trim()
+        .isLength({min: 1})
+        .escape()
+        .withMessage("Post text must be specified"),
+    // Process request after validation and santisation
+    asyncHandler(async(req, res, next) => {
+        // Extract the validation errors from a request
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({
+                status: "error",
+                message: "Validation failed",
+                error: errors.array(),
+            });
+        }
+
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            const err = new Error('Post not found');
+            err.status = 404;
+            return next(err);
+        }
+
+            // Post update
+            const updatePostData = {
+                title: req.body.title || post.title,
+                text: req.body.text || post.text,
+                author: {
+                    _id: req.user.id,
+                    firstName: req.user.firstName,
+                    surname: req.user.surname
+                }, 
+            }
+
+            // Update Post
+            const updatedPost = await Post.findByIdAndUpdate(req.params.id, updatePostData, {new: true})
+                .populate({
+                    path: 'author',
+                    select: '_id firstName surname fullName'
+                })
+                .exec();
+
+            if(!updatedPost) {
+                const err = new Error('Post not found');
+                err.status = 404;
+                return next(err)
+            }
+
+            return res.status(200).json({
+                status: "success",
+                message: "Post updated successfully",
+                post: updatedPost,
+            })
+        
+    })
+];
