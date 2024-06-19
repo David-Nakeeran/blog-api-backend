82a61830c866924cad3964dcd8c8025a3b3d19f3
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-19 12:48:07 +0100

    Refactored error handler to return detailed error information in development mode and simplified in  production.

--- app.js
+++ app.js
@@ -52,13 +52,19 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+
+  if(req.app.get('env') === 'development') {
+    res.json({
+      message: err.message,
+      error: err,
+    });
+  } else {
+    res.json({
+      message: err.message,
+      error: {},
+    });
+  }
 });
 
 module.exports = app;
