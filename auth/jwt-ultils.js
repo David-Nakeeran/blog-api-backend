8ad81dd147848305bbea1da47b53e28ee54a4b1c
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-21 13:19:43 +0100

    Refactored all functions into function expressions . Replace error handling with error helper function for verifyToken and extractToken.

--- auth/jwt-ultils.js
+++ auth/jwt-ultils.js
@@ -1,32 +1,34 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const asyncHandler = require('express-async-handler');
+const {createCustomError} = require('../utils/errorHelpers');
 
-function generateToken(user) {
+const generateToken = function(user) {
     const payload = {id: user._id, admin: user.admin}
     return jwt.sign(payload, process.env.TOKEN_SECRET, {expiresIn: '1m'})
 };
 
-async function verifyToken(token) {
+const verifyToken = async function(token) {
     try {
         const decoded = await jwt.verify(token, process.env.TOKEN_SECRET);
         return decoded;
     }   catch(err) {
-        throw new Error('Token verification failed');
+        return (createCustomError('Token verification failed', 401));
     };
 };
 
-function extractToken(req, res, next) {
+const extractToken = asyncHandler(async(req, res, next) =>  {
     
         const bearerHeader = req.headers['authorization'];
         if(!bearerHeader) {
-            throw new Error('Authorization header is missing');
+            return next(createCustomError('Authorization header is missing', 400));
         }
         const token = bearerHeader.split(' ')[1];
         if(!token) {
-            throw new Error('Token is missing')
+            return next(createCustomError('Token is missing', 400));
         }
 
         return token
-};
+});
 
 module.exports = {generateToken, verifyToken, extractToken};
