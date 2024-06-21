595790991744aba3e20298ad5da37d7dd2bb5066
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-06-21 14:08:14 +0100

    Added cors and method-override

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
+const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
@@ -39,6 +41,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up cors
+app.use(cors());
+
+// MethodOveride - Override with POST having ?_method=VERB
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/', authRouter);
